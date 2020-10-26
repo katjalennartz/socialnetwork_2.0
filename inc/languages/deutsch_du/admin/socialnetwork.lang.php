4fba1ce12adf6f8d9fb0709c22bf51db51a84682
Katja <katja.lennartz@web.de>
2020-10-26 20:15:15 +0100

    Update socialnetwork.lang.php
    add some more language variables to german version. (the other are going to follow)

--- inc/languages/deutsch_du/admin/socialnetwork.lang.php
+++ inc/languages/deutsch_du/admin/socialnetwork.lang.php
@@ -7,11 +7,25 @@ $l['socialnetwork_title'] = "Social Network 2.0 for MyBB";
 $l['socialnetwork_desc'] = "Soziales Netzwerk für die Charaktere.";
 
 $l['socialnetwork_settings_title'] = "Soziales Netzwerk Einstellungen";
-$l['socialnetwork_settings_desc'] = "Einstellungen für das soziale Netzwerk".
+$l['socialnetwork_settings_desc'] = "Einstellungen für das soziale Netzwerk";
+
+$l['socialnetwork_settings_html']='Dürfen Mitglieder HTML verwenden?';
+$l['socialnetwork_settings_mybbcode']='Dürfen Mitglieder MyBB Code verwenden?';
+$l['socialnetwork_settings_img']='Dürfen Mitglieder Bilder verwenden?';
+$l['socialnetwork_settings_badwords']='Soll nach verbotenen Wörtern gefiltert werden?';
+$l['socialnetwork_settings_video']='Dürfen Mitglieder Videos verwenden?';
+$l['socialnetwork_settings_defavatar']='Adresse zu einem Defaultavatar, wenn der Nutzer keins ausgewählt hat oder gelöscht worden ist.?';
+$l['socialnetwork_settings_pn']='Benachrichtigung der Mitglieder (bei Post, Antwort, Like, Freundanfrage etc) per PN?';
+$l['socialnetwork_settings_alert']='Benachrichtigung der Mitglieder (bei Post, Antwort, Like, Freundanfrage etc) per MyAlert(Plugin muss installiert sein!)?';
+
+
 
 
 $l['socialnetwork_perm'] = "Soziales Netzwerk Zugriff";
 $l['socialnetwork_perm_base'] = "Darf die Seiten des sozialen Netzwerks sehen?";
 $l['socialnetwork_perm_edit'] = "Darf die Seite bearbeiten?";
 $l['socialnetwork_perm_mod'] = "Darf das soziale Netzwerk moderieren?";
+
+$l['socialnetwork_tplgroup'] = "socialnetwork";
+
 ?>
