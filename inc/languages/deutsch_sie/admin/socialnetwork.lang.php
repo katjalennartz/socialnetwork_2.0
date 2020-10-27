0f0ac2f9f2d74cfce13bd0179db15b6c3f213841
Katja <katja.lennartz@web.de>
2020-10-27 13:36:11 +0100

    Update socialnetwork.lang.php
    changes in language pack deutsch_sie

--- inc/languages/deutsch_sie/admin/socialnetwork.lang.php
+++ inc/languages/deutsch_sie/admin/socialnetwork.lang.php
@@ -3,15 +3,39 @@
  * Social Network - Administrator Language File
  */
 
-$l['socialnetwork_title'] = "Social Network 2.0 für MyBB";
+$l['socialnetwork_title'] = "Soziales Netzwerk 2.0 für MyBB";
 $l['socialnetwork_desc'] = "Soziales Netzwerk für die Charaktere.";
+$l['socialnetwork_infoolddata'] = "Daten von 1.0 übertragen.";
+$l['socialnetwork_infoacp'] = "Verwaltung der Felder";
 
 $l['socialnetwork_settings_title'] = "Soziales Netzwerk Einstellungen";
-$l['socialnetwork_settings_desc'] = "Einstellungen für das soziale Netzwerk".
+$l['socialnetwork_settings_desc'] = "Einstellungen für das soziale Netzwerk";
+
+$l['socialnetwork_settings_html']='Dürfen Mitglieder HTML verwenden?';
+$l['socialnetwork_settings_mybbcode']='Dürfen Mitglieder MyBB Code verwenden?';
+$l['socialnetwork_settings_img']='Dürfen Mitglieder Bilder verwenden?';
+$l['socialnetwork_settings_badwords']='Soll nach verbotenen Wörtern gefiltert werden?';
+$l['socialnetwork_settings_video']='Dürfen Mitglieder Videos verwenden?';
+$l['socialnetwork_settings_defavatar']='Adresse zu einem Defaultavatar, wenn der Nutzer keins ausgewählt hat oder gelöscht worden ist.?';
+$l['socialnetwork_settings_pn']='Benachrichtigung der Mitglieder (bei Post, Antwort, Like, Freundanfrage etc) per PN?';
+$l['socialnetwork_settings_alert']='Benachrichtigung der Mitglieder (bei Post, Antwort, Like, Freundanfrage etc) per MyAlert(Plugin muss installiert sein!)?';
 
 
-$l['socialnetwork_perm'] = "Soziales Netzwerk Zugriff";
+
+$l['socialnetwork_perm'] = "Soziales Netzwerk";
 $l['socialnetwork_perm_base'] = "Darf die Seiten des sozialen Netzwerks sehen?";
-$l['socialnetwork_perm_edit'] = "Darf die Seite bearbeiten?";
+$l['socialnetwork_perm_edit'] = "Darf die eine Seite erstellen und sie bearbeiten?";
 $l['socialnetwork_perm_mod'] = "Darf das soziale Netzwerk moderieren?";
+
+$l['socialnetwork_module_title'] = "Soziales Netzwerk Felder Verwaltung";
+$l['socialnetwork_module_descr'] = "Hier können Sie weitere Felder hinzufügen.";
+$l['socialnetwork_module_contitle'] = "Soziales Netzwerk Verwaltung";
+$l['socialnetwork_module_explanation'] = "<label>Achtung</label> <div class=\"description\">Bitte mit Vorsicht benutzen. 
+Wenn Sie die Felder ändern und Felder rausgenommen werden, wird der Inhalt bei allen Miglieder gelöscht! Die Standardfelder (Nickname, Avatar, Beschreibung) können nicht geändert werden.
+Anwendung: Felder mit , getrennt ins Textfeld schreiben. Achtung, vorne und hinten muss auch ein Komma stehen, keine Sonderzeichen.<br/>
+<b>Beispiel:</b> ,Status,Wohnohrt,Geburtstag,</div>";
+
+
+$l['socialnetwork_tplgroup'] = "socialnetwork";
+
 ?>
